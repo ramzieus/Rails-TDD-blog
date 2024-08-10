c39c4396fd9a421900ecf73d8cab666ba16cbf82
Ramzi Hemadou <ramzihemadou@gmail.com>
2024-08-10 17:00:38 +0100

    Refactoring specs

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  let(:page) { build(:page) }
   subject { build(:page) }
 
-  it 'has a valid factory' do
-    expect(page).to be_valid
-  end
-
   describe 'validations' do
+    it { is_expected.to be_valid }
     it { is_expected.to belong_to(:user) }
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_uniqueness_of(:title) }
