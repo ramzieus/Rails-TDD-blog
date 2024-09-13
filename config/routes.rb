0fa7a38d192b4623967d0b6c8dac659fa773da8b
Ramzi Hemadou <ramzihemadou@gmail.com>
2024-09-13 19:25:03 +0100

    Add search form, spec

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
       to: 'pages#show',
       slug: /[-a-z0-9+]*/,
       as: :page
+  get '/search', to: 'search#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
