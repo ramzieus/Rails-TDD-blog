41cd02fcf274a0b74325b1200c8b741de4b20cda
Ramzi Hemadou <ramzihemadou@gmail.com>
2024-10-26 13:04:00 +0100

    Add archives view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
       slug: /[-a-z0-9+]*/,
       as: :page
   get '/search', to: 'search#index'
+
+  get 'search/:year/:month',
+      to: 'search#index',
+      year: /\d{4}/,
+      month: /\d{2}/
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
